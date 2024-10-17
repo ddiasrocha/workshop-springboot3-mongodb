28160d935dc0469147146dcb5fb1c17a10eff382
David Dias <ddiasrocha@hotmail.com>
2024-10-17 03:04:38 -0300

    Referenciando os posts de Usuario

--- src/main/java/com/david/workshopmongo/domain/User.java
+++ src/main/java/com/david/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.david.workshopmongo.domain;
 
 import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)//lazy impõe que os posts não serão carregados, serão somente se forem diretamente requisitados, diretamente acessados
+    private List<Post> posts = new ArrayList<>();
 
     private User(){
     }
@@ -46,6 +52,14 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
